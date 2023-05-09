218292eae0e3409bb45c1dbbe7fd7aaa4afea382
Vaishnavigupta97 <vaishnavigupta6389@gmail.com>
2023-05-09 17:19:39 +0530

    define querySelector

--- git/gitEx.js
+++ git/gitEx.js
@@ -24,14 +24,19 @@
 // header.style.borderBottom = "solid 3px #000";
 
 // ..........................................getElementByClassName...........................
-var items = document.getElementsByClassName("list-group-item");
-console.log(items);
-console.log(items[1]);
-items[1].textContent = "hello 2";
-items[1].style.fontWeight = 'bold';
-items[1].style.backgroundColor = 'yellow';
+// var items = document.getElementsByClassName("list-group-item");
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = "hello 2";
+// items[1].style.fontWeight = 'bold';
+// items[1].style.backgroundColor = 'yellow';
 
-for(var i = 0; i <items.length; i++){
-    items[i].style.backgroundColor = '#f4f4f4';
-}
+// for(var i = 0; i <items.length; i++){
+//     items[i].style.backgroundColor = '#f4f4f4';
+// }
+
+//......................................... querySelector...............................
+var header = document.querySelector("#main-header");
+header.style.borderBottom = 'solid 4px #ccc';
+console.log(document.getElementsByTagName('div'));
 
