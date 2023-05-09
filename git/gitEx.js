03bd5becd74fa855b8e9319f7c44170179eab527
Vaishnavigupta97 <vaishnavigupta6389@gmail.com>
2023-05-09 18:59:19 +0530

    define querySelectorAll

--- git/gitEx.js
+++ git/gitEx.js
@@ -36,7 +36,36 @@
 // }
 
 //......................................... querySelector...............................
-var header = document.querySelector("#main-header");
-header.style.borderBottom = 'solid 4px #ccc';
-console.log(document.getElementsByTagName('div'));
+// var header = document.querySelector("#main-header");
+// header.style.borderBottom = 'solid 4px #ccc';
+// console.log(document.getElementsByTagName('div'));
 
+var input = document.querySelector('input');
+input.value = "Hello world";
+
+var submit = document.querySelector('input[type="submit"]');
+submit.value = "SEND";
+
+var item = document.querySelector('.list-group-item');
+item.style.color = "red";   
+
+var lastItem = document.querySelector('.list-group-item:last-child');
+lastItem.style.color = 'blue';
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.color = 'green';
+
+var thirdItem = document.querySelector('.list-group-item:nth-child(2)');
+thirdItem.style.display = "none";
+
+// .......................................querySelectorAll............................
+var titles = document.querySelectorAll('.title');
+console.log(titles);
+titles[0].textContent = "Hello";
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var even = document.querySelectorAll('li:nth-child(even)');
+for(let i = 0; i < even.length; i++){
+    odd[i].style.backgroundColor = "green";
+    even[i].style.backgroundColor = "#ccc";
+}
